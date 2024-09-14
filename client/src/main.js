d01b9e82604159cd78e1feb634f0af15513e2b42
Maxim Kryuchkov <maxim.kryuchkov1@gmail.com>
2024-09-14 12:59:26 +0300

    some changes

--- client/src/main.js
+++ client/src/main.js
@@ -16,27 +16,9 @@ import Router from "@/router";
 import Axios from "axios";
 import {store} from "@/stores/store"
 
-Axios.defaults.baseURL = "http://localhost:8080"
+Axios.defaults.baseURL = "http://localhost:8081/api/"
 
 const app = createApp(App)
-
-// app.config.globalProperties.$vuetify.theme = {
-//   themes: {
-//     light: {
-//       primary: colors.purple,
-//       secondary: colors.grey.darken1,
-//       accent: colors.shades.black,
-//       error: colors.red.accent3,
-//       background: colors.indigo.lighten5, // Not automatically applied
-//       // ...
-//     },
-//     dark: {
-//       primary: colors.blue.lighten3,
-//       background: colors.indigo.base, // If not using lighten/darken, use base to return hex
-//       // ...
-//     },
-//   },
-// }
 
 app.config.globalProperties.$router = Router;
 app.config.globalProperties.$username = '';
