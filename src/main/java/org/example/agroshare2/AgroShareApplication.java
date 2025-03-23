2241a6c4ac4b0d280d45c9ef376c3f682a3f978d
Maxim Kryuchkov <maxim.kryuchkov1@gmail.com>
2025-03-23 21:41:18 +0300

    подтянул liquibase

--- src/main/java/org/example/agroshare2/AgroShareApplication.java
+++ src/main/java/org/example/agroshare2/AgroShareApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AgroShare2Application {
+public class AgroShareApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AgroShare2Application.class, args);
+        SpringApplication.run(AgroShareApplication.class, args);
     }
 
 }
